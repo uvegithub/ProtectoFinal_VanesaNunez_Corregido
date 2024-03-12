da955cdd895510a6ebc9dd5c9dc8a245cfa98bb7
Vanesa Nunez <vanesanunez90@gmail.com>
2024-03-12 17:52:25 +0100

    actu 12_3 1

---
+++ app/src/main/java/com/vanesanunez/practicafinal/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.vanesanunez.practicafinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ref= FirebaseDatabase.getInstance().getReference()
+
+        ref.child("Hola").setValue("Hola desde Kotlin")
+
+    }
+}
